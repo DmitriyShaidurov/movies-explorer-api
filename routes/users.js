0ed925327160db980fb21440391c43c311800542
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2022-12-07 08:40:19 +0500

    try101

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
+const {
+  updateUser, getUser,
+} = require('../controllers/users');
+
+router.get('/users/me', getUser);
+router.patch('users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+
+module.exports = router;
---
+++ utils/const.js
@@ -0,0 +1,19 @@
+const allowedCors = [
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'localhost:3000',
+  'localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+  'localhost:3001',
+  'localhost:3001',
+  'https://api.dmitriysh.nomoredomains.club/',
+  'http://api.dmitriysh.nomoredomains.club/',
+];
+
+const MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb';
+
+export default {
+  allowedCors,
+  MONGO_URL,
+};
