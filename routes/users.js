b4c0d267165feda51ac9d1289308bf17d999e4a7
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2022-12-08 17:04:48 +0500

    patch

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const {
 } = require('../controllers/users');
 
 router.get('/users/me', getUser);
-router.patch('users/me', celebrate({
+router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
